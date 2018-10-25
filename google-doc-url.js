65ce19a56c87718f2dc297dc87118fbc1bde6249
doris0221 <yunzhuo.zhang@trincoll.edu>
2018-10-25 11:37:47 -0400

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ZxvU8eGyuN9M8GxTU9acKVJv70iC3px_m3EVFsOHN9g/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1o77_XGl8Mgz1LjfJuU0PI_IitjP1kg189ryxetEvRSM/edit#gid=0';
